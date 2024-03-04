cc1f70cb733bc6ca3d693c54fd6839e19e5db115
dnomjr <dano.majer@protonmail.com>
2024-03-04 22:01:58 +1300

    added search input

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,16 @@
+import Search from './Search';
+import ThemeSwitchButton from './ThemeSwitchButton';
+import LoginButton from './LoginButton';
+
 const Header = () => {
   return (
-    <div className="col-start-2 col-end-3 border-b-2 border-border ">
-      Header
-    </div>
+    <header className="col-start-2 col-end-3 flex flex-row justify-between border-b-2 border-border p-10 ">
+      <Search />
+      <div className="ml-10 flex flex-row">
+        <ThemeSwitchButton />
+        <LoginButton />
+      </div>
+    </header>
   );
 };
 export default Header;
--- src/ui/Nav.jsx
+++ src/ui/Nav.jsx
@@ -5,40 +5,36 @@ import { TbMovie } from 'react-icons/tb';
 import { PiTelevisionBold } from 'react-icons/pi';
 import { RiLoginCircleLine } from 'react-icons/ri';
 
-const linkClass = 'transition-all hover:text-red-500';
-const iconClass = 'm-auto h-8 w-auto';
-const nameClass = 'text-[.70rem]';
-
 const Nav = () => {
   return (
     <nav className="mt-6">
       <ul className="space-y-8 text-center">
         <li>
-          <NavLink to="/" className={linkClass}>
-            <LuLayoutDashboard className={iconClass} />
-            <span className={nameClass}>Home</span>
+          <NavLink to="/" className="linkClass">
+            <LuLayoutDashboard className="iconClass" />
+            <span className="nameClass">Home</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="movies" className={linkClass}>
-            <TbMovie className={iconClass} />
-            <span className={nameClass}>Movies</span>
+          <NavLink to="movies" className="linkClass">
+            <TbMovie className="iconClass" />
+            <span className="nameClass">Movies</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="series" className={linkClass}>
-            <PiTelevisionBold className={iconClass} />
-            <span className={nameClass}>TV Series</span>
+          <NavLink to="series" className="linkClass">
+            <PiTelevisionBold className="iconClass" />
+            <span className="nameClass">TV Series</span>
           </NavLink>
         </li>
         <li className="last:pt-10">
-          <NavLink to="login" className={linkClass}>
-            <RiLoginCircleLine className={iconClass} />
-            <span className={nameClass}>Login</span>
+          <NavLink to="login" className="linkClass">
+            <RiLoginCircleLine className="iconClass" />
+            <span className="nameClass">Login</span>
           </NavLink>
         </li>
       </ul>
     </nav>
   );
 };
-export default Nav
+export default Nav;
